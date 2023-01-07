c32820e9698b4b006d6dbaff798f2dab93490b3b
Kausar Patherya <52235659+kpath1999@users.noreply.github.com>
2023-01-07 16:25:59 -0600

    modified config file

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,23 +2,20 @@ terraform {
     required_providers {
         aws = {
             source = "hashicorp/aws"
-            version = "~> 4.21.0"
         }
         random = {
             source = "hashicorp/random"
-            version = "~> 3.3.0"
         }
         archive = {
             source = "hashicorp/archive"
-            version = "~> 2.2.0"
         }
     }
-    
+
     required_version = "~> 1.0"
 
     backend "s3" {
         bucket = "tf-remote-state-orangutan"
-        key    = "arn:aws:s3:::tf-remote-state-orangutan"
+        key    = "terraform.tfstate"
         region = "us-east-1"
     }
 }
