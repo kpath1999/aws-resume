65ee4a3ea9a51b893e2af6ebbe57831675bf3640
Kausar Patherya <52235659+kpath1999@users.noreply.github.com>
2023-01-07 19:26:31 -0600

    empty directory?

--- terraform/0-provider.tf
+++ terraform/0-provider.tf
@@ -1,11 +1,3 @@
 terraform {
-    backend "s3" {
-        bucket = "tf-remote-state-orangutan"
-        key    = "terraform.tfstate"
-        region = "us-east-1"
-    }
-}
-
-provider "aws" {
-    region  = "us-east-1"
+    backend "s3" {}
 }
